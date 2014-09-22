4f8e428503c199839b877a9928ce5464bccba83a
Robert <abewm@blogs.itz.uni-halle.de>
2014-09-22 12:27:44 +0200

    new theme for "sprechbuehne"-website

---
+++ wp-content/themes/sprechbuehne/footer.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains footer content and the closing of the #main and #page div elements.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twelve
+ * @since Twenty Twelve 1.0
+ */
+?>
+	</div><!-- #main .wrapper -->
+	<footer id="colophon" role="contentinfo">
+		<div class="site-info">
+			© 2013-<?php echo date('Y'); ?> • <a href="mailto:sprechbuehne@sprechwiss.uni-halle.de">E-Mail</a> • <a href="?page_id=28">Impressum</a> 
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+</body>
+</html>
