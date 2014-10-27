6e5312b88af17c1ba076b55900b7ed3f3c8c1705
root <root@blogs.itz.uni-halle.de>
2014-10-27 15:00:59 +0100

    passing transport headers through now

--- prox-mlu.php
+++ prox-mlu.php
@@ -13,10 +13,19 @@ if(!function_exists('get_url')) {
     $timeout = 5;
     curl_setopt($ch, CURLOPT_URL, $url);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_HEADER, 1);
     curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
     $data = curl_exec($ch);
+    list($header, $body) = explode("\r\n\r\n", $data, 2);
+    $headers=explode("\n",$header);
+    $headersToSend=['Content-Type','Content-Length','Last-Modified','HTTP'];
+    array_walk($headers,function($h)use($headersToSend){
+      foreach($headersToSend as $send) {
+        if(stripos($h,$send)===0) return header($h);
+      }
+    });
     curl_close($ch);
-    return $data;
+    return $body;
   }
 }
 
