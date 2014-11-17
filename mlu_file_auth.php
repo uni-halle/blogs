13d8d5c06bea1cf6edfb4237672743b743957cf6
Robert <robert.jaeckel@itz.uni-halle.de>
2014-11-17 15:41:57 +0100

    updated for better usability

--- mlu_file_auth.php
+++ mlu_file_auth.php
@@ -1,6 +1,22 @@
 <?php
 require_once('wp-load.php');
 
-is_user_logged_in() || auth_redirect();
+call_user_func(function($l){
+  $check=function() {
+    if(!is_user_logged_in()) return false;
+    $u = wp_get_current_user();
+    return is_super_admin()||count(wp_get_current_user()->allcaps)>0;
+  };
+  $quit = function() {
+    header('location: '.home_url());
+    auth_redirect();
+    die();
+    return true;
+  };
+  $check()||$quit();
+  require_once $l;
+  return 1;
+},'wp-includes/ms-files.php');
 
-require_once "wp-includes/ms-files.php";
+
+
