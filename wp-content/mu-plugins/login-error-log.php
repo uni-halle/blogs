4675d33cdc0ef1402bf7519c8a7688c4b1afbcaa
Robert <robert.jaeckel@itz.uni-halle.de>
2015-08-05 17:14:51 +0200

    login logging to at least get track of hackers

---
+++ wp-content/mu-plugins/login-error-log.php
@@ -0,0 +1,42 @@
+<?php
+
+
+
+add_action( 'wp_login_failed', function($user){
+	$keys = array(
+		'REQUEST_TIME'=>'time',
+		'REMOTE_ADDR'=>'client',
+		'REQUEST_METHOD'=>'method',
+		'HTTPS'=>'ssl',
+		'HTTP_HOST'=>'host',
+		'REQUEST_URI'=>'uri'
+	);
+	foreach ($keys as $k=>$v) $$v=$_SERVER[$k];
+	$logline=implode(' ',array(
+		date('c',$time),$user,
+		'FROM',$client,
+		$method,'http'.($ssl?'s':'').'://'.$host.$uri
+	));
+	$week=date('W',$time);
+	`echo $logline >>\~logins/wp-login-$week.err.log`;
+} );
+
+add_action( 'wp_login', function($user){
+        $keys = array(
+                'REQUEST_TIME'=>'time',
+                'REMOTE_ADDR'=>'client',
+                'REQUEST_METHOD'=>'method',
+                'HTTPS'=>'ssl',
+                'HTTP_HOST'=>'host',
+                'REQUEST_URI'=>'uri'
+        );
+        foreach ($keys as $k=>$v) $$v=$_SERVER[$k];
+        $logline=implode(' ',array(
+                date('c',$time),$user,
+                'FROM',$client,
+                $method,'http'.($ssl?'s':'').'://'.$host.$uri
+        ));
+        $week=date('W',$time);
+        `echo $logline >>\~logins/wp-login-$week.success.log`;
+} );
+
