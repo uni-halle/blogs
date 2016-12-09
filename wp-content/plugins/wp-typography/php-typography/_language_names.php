33e27f3f98b73bd3b7480522a4fd5ba0375ef714
Robert <robert.jaeckel@itz.uni-halle.de>
2016-12-09 13:42:14 +0100

    updated plugin wp-typography to 3.4.0

--- wp-content/plugins/wp-typography/php-typography/_language_names.php
+++ wp-content/plugins/wp-typography/php-typography/_language_names.php
@@ -1,53 +1,53 @@
-<?php __( 'Afrikaans', 'wp-typography' ); ?>
-<?php __( 'Bulgarian', 'wp-typography' ); ?>
-<?php __( 'Catalan', 'wp-typography' ); ?>
-<?php __( 'Czech', 'wp-typography' ); ?>
-<?php __( 'Welsh', 'wp-typography' ); ?>
-<?php __( 'Danish', 'wp-typography' ); ?>
-<?php __( 'German (Traditional)', 'wp-typography' ); ?>
-<?php __( 'German', 'wp-typography' ); ?>
-<?php __( 'Greek (Modern Monotonic)', 'wp-typography' ); ?>
-<?php __( 'Greek (Modern Polytonic)', 'wp-typography' ); ?>
-<?php __( 'English (United Kingdom)', 'wp-typography' ); ?>
-<?php __( 'English (United States)', 'wp-typography' ); ?>
-<?php __( 'Spanish', 'wp-typography' ); ?>
-<?php __( 'Estonian', 'wp-typography' ); ?>
-<?php __( 'Basque', 'wp-typography' ); ?>
-<?php __( 'Finnish', 'wp-typography' ); ?>
-<?php __( 'French', 'wp-typography' ); ?>
-<?php __( 'Irish', 'wp-typography' ); ?>
-<?php __( "Galician", 'wp-typography' ); ?>
-<?php __( 'Greek (Ancient)', 'wp-typography' ); ?>
-<?php __( 'Croatian', 'wp-typography' ); ?>
-<?php __( 'Hungarian', 'wp-typography' ); ?>
-<?php __( 'Armenian', 'wp-typography' ); ?>
-<?php __( 'Interlingua', 'wp-typography' ); ?>
-<?php __( 'Indonesian', 'wp-typography' ); ?>
-<?php __( 'Icelandic', 'wp-typography' ); ?>
-<?php __( 'Italian', 'wp-typography' ); ?>
-<?php __( 'Georgian', 'wp-typography' ); ?>
-<?php __( 'Latin (Classical)', 'wp-typography' ); ?>
-<?php __( 'Latin', 'wp-typography' ); ?>
-<?php __( 'Lithuanian', 'wp-typography' ); ?>
-<?php __( 'Latvian', 'wp-typography' ); ?>
-<?php __( 'Mongolian (Cyrillic)', 'wp-typography' ); ?>
-<?php __( 'Dutch', 'wp-typography' ); ?>
-<?php __( "Norwegian", 'wp-typography' ); ?>
-<?php __( 'Polish', 'wp-typography' ); ?>
-<?php __( 'Portuguese', 'wp-typography' ); ?>
-<?php __( 'Romanian', 'wp-typography' ); ?>
-<?php __( 'Russian', 'wp-typography' ); ?>
-<?php __( 'Sanskrit', 'wp-typography' ); ?>
-<?php __( "Serbocroatian (Cyrillic)", 'wp-typography' ); ?>
-<?php __( 'Serbocroatian (Latin)', 'wp-typography' ); ?>
-<?php __( 'Slovak', 'wp-typography' ); ?>
-<?php __( 'Slovenian', 'wp-typography' ); ?>
-<?php __( 'Serbian (Cyrillic)', 'wp-typography' ); ?>
-<?php __( 'Swedish', 'wp-typography' ); ?>
-<?php __( 'Thai', 'wp-typography' ); ?>
-<?php __( 'Turkmen', 'wp-typography' ); ?>
-<?php __( 'Turkish', 'wp-typography' ); ?>
-<?php __( "Ukrainian", 'wp-typography' ); ?>
-<?php __( 'Chinese pinyin (Latin)', 'wp-typography' ); ?>
-<?php __( 'German', 'wp-typography' ); ?>
-<?php __( 'English (United States)', 'wp-typography' ); ?>
+<?php _x( 'Afrikaans', 'language name', 'wp-typography' ); ?>
+<?php _x( 'Bulgarian', 'language name', 'wp-typography' ); ?>
+<?php _x( 'Catalan', 'language name', 'wp-typography' ); ?>
+<?php _x( 'Czech', 'language name', 'wp-typography' ); ?>
+<?php _x( 'Welsh', 'language name', 'wp-typography' ); ?>
+<?php _x( 'Danish', 'language name', 'wp-typography' ); ?>
+<?php _x( 'German (Traditional)', 'language name', 'wp-typography' ); ?>
+<?php _x( 'German', 'language name', 'wp-typography' ); ?>
+<?php _x( 'Greek (Modern Monotonic)', 'language name', 'wp-typography' ); ?>
+<?php _x( 'Greek (Modern Polytonic)', 'language name', 'wp-typography' ); ?>
+<?php _x( 'English (United Kingdom)', 'language name', 'wp-typography' ); ?>
+<?php _x( 'English (United States)', 'language name', 'wp-typography' ); ?>
+<?php _x( 'Spanish', 'language name', 'wp-typography' ); ?>
+<?php _x( 'Estonian', 'language name', 'wp-typography' ); ?>
+<?php _x( 'Basque', 'language name', 'wp-typography' ); ?>
+<?php _x( 'Finnish', 'language name', 'wp-typography' ); ?>
+<?php _x( 'French', 'language name', 'wp-typography' ); ?>
+<?php _x( 'Irish', 'language name', 'wp-typography' ); ?>
+<?php _x( 'Galician', 'language name', 'wp-typography' ); ?>
+<?php _x( 'Greek (Ancient)', 'language name', 'wp-typography' ); ?>
+<?php _x( 'Croatian', 'language name', 'wp-typography' ); ?>
+<?php _x( 'Hungarian', 'language name', 'wp-typography' ); ?>
+<?php _x( 'Armenian', 'language name', 'wp-typography' ); ?>
+<?php _x( 'Interlingua', 'language name', 'wp-typography' ); ?>
+<?php _x( 'Indonesian', 'language name', 'wp-typography' ); ?>
+<?php _x( 'Icelandic', 'language name', 'wp-typography' ); ?>
+<?php _x( 'Italian', 'language name', 'wp-typography' ); ?>
+<?php _x( 'Georgian', 'language name', 'wp-typography' ); ?>
+<?php _x( 'Latin (Classical)', 'language name', 'wp-typography' ); ?>
+<?php _x( 'Latin', 'language name', 'wp-typography' ); ?>
+<?php _x( 'Lithuanian', 'language name', 'wp-typography' ); ?>
+<?php _x( 'Latvian', 'language name', 'wp-typography' ); ?>
+<?php _x( 'Mongolian (Cyrillic)', 'language name', 'wp-typography' ); ?>
+<?php _x( 'Dutch', 'language name', 'wp-typography' ); ?>
+<?php _x( 'Norwegian', 'language name', 'wp-typography' ); ?>
+<?php _x( 'Polish', 'language name', 'wp-typography' ); ?>
+<?php _x( 'Portuguese', 'language name', 'wp-typography' ); ?>
+<?php _x( 'Romanian', 'language name', 'wp-typography' ); ?>
+<?php _x( 'Russian', 'language name', 'wp-typography' ); ?>
+<?php _x( 'Sanskrit', 'language name', 'wp-typography' ); ?>
+<?php _x( 'Serbocroatian (Cyrillic)', 'language name', 'wp-typography' ); ?>
+<?php _x( 'Serbocroatian (Latin)', 'language name', 'wp-typography' ); ?>
+<?php _x( 'Slovak', 'language name', 'wp-typography' ); ?>
+<?php _x( 'Slovenian', 'language name', 'wp-typography' ); ?>
+<?php _x( 'Serbian (Cyrillic)', 'language name', 'wp-typography' ); ?>
+<?php _x( 'Swedish', 'language name', 'wp-typography' ); ?>
+<?php _x( 'Thai', 'language name', 'wp-typography' ); ?>
+<?php _x( 'Turkmen', 'language name', 'wp-typography' ); ?>
+<?php _x( 'Turkish', 'language name', 'wp-typography' ); ?>
+<?php _x( 'Ukrainian', 'language name', 'wp-typography' ); ?>
+<?php _x( 'Chinese pinyin (Latin)', 'language name', 'wp-typography' ); ?>
+<?php _x( 'German', 'language name', 'wp-typography' ); ?>
+<?php _x( 'English (United States)', 'language name', 'wp-typography' ); ?>
