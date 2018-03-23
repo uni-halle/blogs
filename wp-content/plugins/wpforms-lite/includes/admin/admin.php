7b66db4bdb9280954859895fae580105966f0e19
rjackel <robert.jaeckel@itz.uni-halle.de>
2018-03-23 11:28:26 +0100

    updated plugin wpforms-lite to 1.4.5.2

--- wp-content/plugins/wpforms-lite/includes/admin/admin.php
+++ wp-content/plugins/wpforms-lite/includes/admin/admin.php
@@ -118,7 +118,7 @@ function wpforms_admin_scripts() {
 	// Main admin script.
 	wp_enqueue_script(
 		'wpforms-admin',
-		WPFORMS_PLUGIN_URL . "assets/js{$dir}/admin{$suffix}.js",
+		WPFORMS_PLUGIN_URL . "assets/js/admin{$suffix}.js",
 		array( 'jquery' ),
 		WPFORMS_VERSION,
 		false
@@ -158,6 +158,7 @@ function wpforms_admin_scripts() {
 		'provider_delete_confirm'         => esc_html__( 'Are you sure you want to disconnect this account?', 'wpforms' ),
 		'provider_auth_error'             => esc_html__( 'Could not authenticate with the provider.', 'wpforms' ),
 		'save_refresh'                    => esc_html__( 'Save and Refresh', 'wpforms' ),
+		'testing'                         => esc_html__( 'Testing', 'wpforms' ),
 		'upgrade_completed'               => esc_html__( 'Upgrade was successfully completed!', 'wpforms' ),
 		'upload_image_title'              => esc_html__( 'Upload or Choose Your Image', 'wpforms' ),
 		'upload_image_button'             => esc_html__( 'Use Image', 'wpforms' ),
